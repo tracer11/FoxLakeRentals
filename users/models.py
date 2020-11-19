f2ca0e317dc1338f9a6b21f4b4c946ce509acf68
Karol Gurba <karolgurba22@gmail.com>
2020-11-19 16:28:20 -0600

    fixed user model bugs

--- users/models.py
+++ users/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils import timezone
 from .managers import CustomUserManager
 # Create your models here.
 
-class CustomUser(AbstractBaseUser):
-  email = models.CharFiled(unique=True)
-  phone = models.IntField(unique=True)
+class CustomUser(AbstractBaseUser,PermissionsMixin):
+  email = models.EmailField(unique=True)
+  phone = models.IntegerField(unique=True)
   is_staff = models.BooleanField(default=False)
   is_active = models.BooleanField(default=True)
   date_joined = models.DateTimeField(default=timezone.now)
