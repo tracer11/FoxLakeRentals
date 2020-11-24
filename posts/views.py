58560b41d82503305c07b2bfa039a298ffcf3c05
Karol Gurba <karolgurba22@gmail.com>
2020-11-23 20:21:27 -0600

    crated detail view

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Post 
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from .models import Post, PostImage 
 
 # Create your views here.
 
 class PostListView(ListView):
     model = Post
     template_name = "TEMPLATE_NAME"
+
+def detailPostView(request, id)
+  post = get_object_or_404(Post, id)
+  photos = PostImage.objects.filter(post=post)
+  return render(request, 'post-detail', {'photos':photos, 'post':post})
+
+
+
+
