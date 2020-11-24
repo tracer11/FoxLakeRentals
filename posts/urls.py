1873df7e5092e4a17b0e9ab2964c6f8cac7a3918
Karol Gurba <karolgurba22@gmail.com>
2020-11-24 12:33:03 -0600

    set up urls and home page

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
+from .views import PostListView
 
 urlpatterns = [
-    path('', ),
+    path('', PostListView.as_view(), name='posts-home'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -6,9 +6,10 @@ from .models import Post, PostImage
 
 class PostListView(ListView):
     model = Post
-    template_name = "TEMPLATE_NAME"
+    template_name = 'posts/home.html'
+    context_object_name = 'posts'
 
-def detailPostView(request, id)
+def detailPostView(request, id):
   post = get_object_or_404(Post, id)
   photos = PostImage.objects.filter(post=post)
   return render(request, 'post-detail', {'photos':photos, 'post':post})
