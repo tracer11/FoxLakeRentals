6535e126d4a4826613d3c014fc3ddf7493ff50fd
Karol Gurba <karolgurba22@gmail.com>
2020-12-01 13:07:55 -0600

    created new forms

--- posts/forms.py
+++ posts/forms.py
@@ -1,3 +1,14 @@
 from django import forms 
 from .models import Post, PostImage
 
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'description', 'rate',)
+
+class PostImageForm(forms.ModelForm):
+    class Meta:
+        model = PostImage
+        fields = ('image',)
+    
+    
