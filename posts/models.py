b71fea275bea21b512ee017b27b388a5bd42fb4e
Karol Gurba <karolgurba22@gmail.com>
2020-12-18 15:51:42 -0600

    overrode save method to resize image

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.conf import settings 
+from django.conf import settings
+from PIL import Image
 # Create your models here.
 class Post(models.Model):
   title = models.CharField(max_length=250)
@@ -12,4 +13,14 @@ class Post(models.Model):
 class PostImage(models.Model):
   post = models.ForeignKey(Post, default=None, on_delete=models.CASCADE)
   image = models.ImageField(upload_to='post_images')
+
+  def save(self, *args, **kwargs):
+    super().save(*args, **kwargs)
+
+    img = Image.open(self.image.path)
+
+    if img.width > 500 or img.height > 500:
+      new_size = (500, 500)
+      img.thumbnail(new_size)
+      img.save(self.image.path)
 
